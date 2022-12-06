3c82beb1c522ef43135f034b1932be439a0ff915
Gypsophila <futur_wxc@163.com>
2022-12-06 09:11:31 +0800

    chore(字体): 引入 vfonts

--- src/plugins/index.ts
+++ src/plugins/index.ts
@@ -1,6 +1,7 @@
 import type { App } from 'vue';
 import '@unocss/reset/tailwind.css';
 import 'uno.css';
+import 'vfonts/FiraSans.css';
 import '@/styles/index.scss';
 
 // eslint-disable-next-line @typescript-eslint/no-empty-function
