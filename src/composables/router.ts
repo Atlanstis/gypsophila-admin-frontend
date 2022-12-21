77b65aecbc7a952ca4fcaa2b4d41838da9a28572
Gypsophila <futur_wxc@163.com>
2022-12-21 15:28:21 +0800

    feat(菜单): 增加点击跳转

---
+++ src/composables/router.ts
@@ -0,0 +1,28 @@
+import { router as globalRouter } from '@/router';
+import type { RouteLocationRaw } from 'vue-router';
+import { useRouter } from 'vue-router';
+/**
+ * 路由跳转
+ * @param inSetup - 是否在vue页面/组件的setup里面调用，在axios里面无法使用useRouter和useRoute
+ */
+export function useRouterPush(inSetup = true) {
+  const router = inSetup ? useRouter() : globalRouter;
+
+  /**
+   * 路由跳转
+   * @param to - 需要跳转的路由
+   * @param newTab - 是否在新的浏览器 Tab 标签打开
+   */
+  function routerPush(to: RouteLocationRaw, newTab?: boolean) {
+    if (newTab) {
+      const routerData = router.resolve(to);
+      window.open(routerData.href, '_blank');
+    } else {
+      router.push(to);
+    }
+  }
+
+  return {
+    routerPush,
+  };
+}
--- src/layouts/basic/components/sider/components/menu.vue
+++ src/layouts/basic/components/sider/components/menu.vue
@@ -1,29 +1,50 @@
 <template>
   <NScrollbar class="flex-1-hidden">
     <NMenu
+      :value="activeKey"
       :collapsed="app.siderCollapse"
       :collapsed-width="sider.collapsedWidth"
       :collapsed-icon-size="22"
       :width="sider.width"
       :options="menus"
       :indent="sider.indent"
+      @update:value="handleUpdateMenu"
     ></NMenu>
   </NScrollbar>
 </template>
 
 <script lang="ts" setup>
 import { useAppStore, useThemeStore, useRouteStore } from '@/store';
+import type { MenuOption } from 'naive-ui';
 import { computed } from 'vue';
+import { useRoute } from 'vue-router';
+import { useRouterPush } from '@/composables';
 
 defineOptions({
   name: 'SiderMenu',
 });
 
+const route = useRoute();
+
 const app = useAppStore();
 const { sider } = useThemeStore();
 const routeStore = useRouteStore();
 
+const { routerPush } = useRouterPush();
+
+/** 展示菜单 */
 const menus = computed(() => routeStore.menus as App.AdminMenuOption[]);
+
+/** 当前选中菜单 */
+const activeKey = computed(() => route.name as string);
+
+/**
+ * 处理菜单点击
+ */
+function handleUpdateMenu(key: string, item: MenuOption) {
+  const menuItem = item as App.AdminMenuOption;
+  routerPush(menuItem.routePath);
+}
 </script>
 
 <style lang="scss" scoped></style>
